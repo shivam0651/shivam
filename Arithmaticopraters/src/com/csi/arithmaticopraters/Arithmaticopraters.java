a8c6cee716927f08d59824bf5bb29c69ff3f9fb5
shivam0651 <DJ SYM@192.168.0.4>
2021-08-13 09:53:13 +0530

    enter value

---
+++ Arithmaticopraters/src/com/csi/arithmaticopraters/Arithmaticopraters.java
@@ -0,0 +1,21 @@
+package com.csi.arithmaticopraters;
+
+public class Arithmaticopraters {
+	public static void main(String[] args) {
+
+		int a = 20, b = 10, result = 0;
+		result = a - b;
+
+		System.out.println("Substraction\n" + result);
+		result = a * b;
+		System.out.println("multiplication\n" + result);
+		result = a / b;
+		System.out.println("division\n" + result);
+	}
+
+	static {
+		int p = 20, q = 10, result = 0;
+		result = p + q;
+		System.out.println("addition of \n" + result);
+	}
+}
